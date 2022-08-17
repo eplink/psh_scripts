6b7c1e178de5562143a4311acf10de5abf9d0424
tibet <tibet@eplink.ru>
2022-08-17 19:44:59 +0500

    Добавлен файл параметров и изменения в скрипт BackupRemoteServers

--- BackupRemoteServers.ps1
+++ BackupRemoteServers.ps1
@@ -1,18 +1,41 @@
 ######################################################################################################################
-###             Скрипт автоматизации резервного копирования удаленных серверов в указанную папку                   ###
-###             BackupRemoteServers.ps1 ver 0.0.1
+###     Скрипт автоматизации резервного копирования удаленных серверов                                             ###
+###                                                                                                                ###
+###     Файлы:  BackupRemoteServers.ps1, BackupRemoteServers.json                                                  ###
+###     Версия: 0.0.1                                                                                              ###
 ######################################################################################################################
 
+$jSonFile = $MyInvocation.MyCommand.Path | Split-Path -Parent
+$jSonFile = $jSonFile + "\BackupRemoteServers.json"
 
-# Параметры 
+$Job = Get-Content $jSonFile -Raw -Encoding UTF8 | ConvertFrom-Json     # Загрузка параметров из .json
 
-$backupPath = '\\192.168.1.167\backup\HPV'  # Путь для резервного копирования
-[string[]] $arrayOfServersName =    'servavir.oblgaz56.local',
-                                    'servavr2.oblgaz56.local'
-Write-Host $backupPath
+foreach ($Server in $Job.Servers) {                                     # Парсинг массива загруженного объекта
+    
+    $strLogFile = $Server.BackupPath -replace '/', '\' 
+    $strLogFile += "\" + $Server.DnsName + ".log"
 
-foreach($serverName in $arrayOfServersName) {  # Основной цикл обработки массива серверов
-    Write-Host $serverName
+    $strBackupParam = " -backupTarget:" + $Server.BackupPath -replace '/', '\'
+    $strBackupParam += $Server.IncludeArg
+    $strBackupParam += " -allCritical"
+    $strBackupParam += " -user:" + $Server.User
+    $strBackupParam += " -password:" + $Server.Password
+    $strBackupParam += " -quiet"
+    
+    $ScriptBlock = [scriptblock]::Create("WBADMIN START BACKUP $strBackupParam") 
+    
+    Invoke-Command -ComputerName $Server.DnsName -ScriptBlock $ScriptBlock | Out-File -LiteralPath $strLogFile
+
+    # WBADMIN START BACKUP $strBackupParam >> $strLogFile
+
+    # $strBackupParam = "-backupTarget:" + $strBackupParam 
+    
+    # WBADMIN START BACKUP -backupTarget:\\192.168.1.167\backup\hpv -include:d: -allCritical -user:OBLGAZ56\AUTOADMIN
+    # -password:autoadmin -quiet
+    
+    # Write-Host $strBackupParam
+    # Write-Host $strLogFile
 }
 
-
+# Write-Host $Job.MailServer
+# Write-Host $Job.MailUser
