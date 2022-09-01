0e74026cb83531faae49743bde09b53a5f0c1ab0
tibet <tibet@eplink.ru>
2022-09-01 17:35:13 +0500

    change folder-backup script

--- Folder-Backup.ps1
+++ Folder-Backup.ps1
@@ -21,18 +21,16 @@
 # to form a string of parameters for the console archiver
 Param ( 
     [string]$target,            # target path for backup
-
     [string]$excList,           # filepath with exclusions from backup
     [string]$dest,              # archive file destination path
     [bool]$addTL,               # added time label to archive name
-    [bool]$isVSS,               # is an shadow copy with VSS
+    [0..5]$CompressRatio,       #     
     [bool]$isIncrCopy           # is an incremental copy
 )
 
 # to use the default parametrs
 $RAR_FILE_PATH = $Env:ProgramFiles + '\WinRAR\rar.exe'
 if (!$addTL) { $addTL = $true }
-if (!$isVSS) { $isVSS = $false }
 if (!$isIncrCopy) { $isIncrCopy = $false }
 
 # to check parameters for errors
@@ -71,21 +69,5 @@ if ($isIncrCopy) {
     $keyIncrCopy = '-ao'
 }
 
-$ModTarget = $target
-if ($isVSS) {
-    $DiskTarget = Split-Path $target -Qualifier
-    $s1 = (gwmi -List Win32_ShadowCopy).Create($DiskTarget, "ClientAccessible")
-    $s2 = gwmi Win32_ShadowCopy | Where-Object { $_.ID -eq $s1.ShadowID }
-    $d  = $s2.DeviceObject + "\"    
-    
-
-}
-
-#Создаем теневую копию
-
-#Создаем на нее ярлык (удалим предыдущий, если остался после прерванной архивации)
-#CMD /C rmdir "$VSCPath"
-#cmd /c mklink /d $VSCPath "$d"
-
-Write-Host $RAR_FILE_PATH 'A' '-inul' '-ac' $keyIncrCopy $logFilePath '-m5' '-os' '-r' '-rr' '-s' '-t' $excludesList `
+& $RAR_FILE_PATH 'A' '-inul' '-ac' $keyIncrCopy $logFilePath '-m3' '-os' '-r' '-rr' '-s' '-t' $excludesList `
     $archFilePath $target
